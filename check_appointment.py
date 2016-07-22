37da7a86adb49ab3b51851aceee36390239c622c
Kushagra Sharma <ksharm01@gmail.com>
2016-07-21 20:27:39 -0700

    finished

--- check_appointment.py
+++ check_appointment.py
@@ -1,7 +1,7 @@
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -32,14 +32,16 @@ def login():
 
 
 def is_valid_appt(driver):
+    print(driver.find_element_by_id("compTableau_Entete").text)
     d = dparser.parse(driver.find_element_by_id("compTableau_Entete").text)
+    print(d)
     return datetime.date.today() <= d <= datetime.date(2016, 8, 1)
 
 
 def check_for_appts():
     try:
         driver = login()
-        for i in range(3):
+        while True:
             try:
                 WebDriverWait(driver, 3).until(EC.alert_is_present(),
                                            'Timed out waiting for PA creation ' +
@@ -48,12 +50,11 @@ def check_for_appts():
                 alert = driver.switch_to_alert()
                 alert.accept()
                 print("alert accepted")
-                os.system('say "an appointment has been found"')
-                input("Press ENTER to continue.")
             except TimeoutException:
                 if is_valid_appt(driver):
-                    os.system('say "an appointment has been found"')
+                    os.system('say "an appointment has been found hella sick"')
                     input("Press ENTER to continue.")
+            sleep(30)
             driver.refresh()
             driver.switch_to.frame(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "BODY_WIN"))))
             driver.switch_to.frame(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "CONTENU_WIN"))))
